5c16799b1dd1c8ae856711df19a60e0c20f6ecbd
Hemendra Singh Rao <hemendra.dtechies@gmail.com>
2022-07-15 19:33:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { useQuery, gql, useLazyQuery,useMutation } from '@apollo/client';
+
+const GETDATA = gql`
+query GetData{
+  allUsers {
+    id
+    name
+    Posts{
+      title
+      views
+      User{
+        name
+      }
+    }
+   
+  }
+}
+`
+
+
+const GETDATASINGLE = gql`
+query ($id:ID!){
+  User(id:$id) {
+    Posts{
+      title
+      views
+    }
+  }
+}
+`
+
+const ADDATA = gql`
+mutation User($name:String!){
+  createUser(name: $name){
+    name:name
+  }
+}
+`
+
+const REMOVEUSER = gql `
+mutation User($id:ID!){
+  removeUser(id:$id){
+    id:id
+  }
+}
+`
+
+const EDITUSER = gql `
+mutation User($name:String!,$id:ID!){
+  updateUser(name:$name,id:$id){
+    name:name
+    id:id
+  }
+}
+`
+
 
 function App() {
+  const [name, setName] = useState('')
+  const { loading, error, data,refetch } = useQuery(GETDATA);
+  const [createUser,{data:response}] = useMutation(ADDATA);
+  const [updateUser,{data:response3}] = useMutation(EDITUSER);
+  const [removeUser,{data:response2}] = useMutation(REMOVEUSER);
+  const [getData1, { data: get }] = useLazyQuery(GETDATASINGLE);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  const handleClick = async(e) => {
+    e.preventDefault()
+    await createUser({variables:{name:name}}).then(()=>{
+      refetch()
+      setName('')
+    }).catch((err)=>console.log(err)) 
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="">
+      <h1>GRAPHQL</h1>
+      <div>
+        <h1>Add Data</h1>
+        <input type='text' value={name} onChange={(e) => setName(e.target.value)} />
+        <button onClick={handleClick}>Add User</button>
+        <button onClick={()=>updateUser({variables:{name:name,id:456}})}>Edit User</button>
+      </div>
+
+
+      <div>
+        {
+          data.allUsers.map((da) => {
+            return (
+              <>
+                {da.name}
+                <button onClick={() => getData1({ variables: { id: da.id } })}>View</button>
+                <button onClick={()=>{
+                  removeUser({ variables: { id: da.id } })
+                  refetch()
+                }}>Delete</button>
+              </>
+            )
+          })
+        }
+      </div>
+      <div>Detail</div>
+      <div>
+        {
+          get?.User?.Posts?.map((d) => {
+            return (
+              <>
+                <p>{d?.title}</p>
+                <p>{d?.views}</p>
+              </>
+            )
+          })
+        }
+      </div>
+
     </div>
   );
 }
